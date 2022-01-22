9fc70ef5f8d067ff03df57a56a1fb0af90843b06
Andy Guice <andy.guice@binderpos.com>
2022-01-22 09:15:50 -0600

    cleanup unused funcs

--- routes/answers.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addAnswer, getAnswers } from "../controllers/answers.js";
-
-const router = express.Router();
-
-router.get("/:formId", getAnswers);
-router.post("/:formId", addAnswer);
-
-export default router;
